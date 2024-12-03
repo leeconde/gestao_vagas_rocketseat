c3cbdea2a5db28388331a9e2271d88a3b11fa0b2
leeconde <lecondesouza@gmail.com>
2024-12-03 19:04:11 -0300

    Deploy

--- gestao_vagas/src/test/java/br/com/ednocel/gestao_vagas/modules/company/controllers/CreateJobControllerTest.java
+++ gestao_vagas/src/test/java/br/com/ednocel/gestao_vagas/modules/company/controllers/CreateJobControllerTest.java
@@ -6,31 +6,31 @@ import br.com.ednocel.gestao_vagas.modules.company.repositories.CompanyRepositor
 import br.com.ednocel.gestao_vagas.utils.TestUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 public class CreateJobControllerTest {
 
-    private MockMvc mvc;
+    public MockMvc mvc;
 
     @Autowired
-    private WebApplicationContext context;
+    public WebApplicationContext context;
 
     @Autowired
-    private CompanyRepository companyRepository;
+    public CompanyRepository companyRepository;
 
     @BeforeEach
     public void setup() {
